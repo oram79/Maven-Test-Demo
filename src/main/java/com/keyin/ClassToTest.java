63718b3bcb413cfb8606e0326761aa888901b807
Logan Oram <162460537+oram79@users.noreply.github.com>
2025-01-27 23:05:28 -0330

    added tests

--- src/main/java/com/keyin/ClassToTest.java
+++ src/main/java/com/keyin/ClassToTest.java
@@ -1,4 +1,17 @@
 package com.keyin;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassToTest {
+    private final List<String> listOfStrings = new ArrayList<String>();
+
+
+    public List<String> methodToTest(String word) {
+        if (!listOfStrings.contains(word.toLowerCase())) {
+            listOfStrings.add(word.toLowerCase());
+        }
+
+        return listOfStrings;
+    }
 }
--- src/test/java/com/keyin/ClassToTestTest.java
+++ src/test/java/com/keyin/ClassToTestTest.java
@@ -1,4 +1,35 @@
 package com.keyin;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ClassToTestTest {
+
+    @Test
+    public void testMethodToTest() {
+        ClassToTest objectUnderTest = new ClassToTest();
+
+        List<String> listOfStrings = objectUnderTest.methodToTest("test");
+
+        Assertions.assertTrue(listOfStrings.contains("test"));
+        Assertions.assertEquals(listOfStrings.size(), 1);
+
+        listOfStrings = objectUnderTest.methodToTest("test");
+
+        Assertions.assertEquals(listOfStrings.size(), 1);
+
+        listOfStrings = objectUnderTest.methodToTest("Test");
+
+        Assertions.assertEquals(listOfStrings.size(), 1);
+
+        listOfStrings = objectUnderTest.methodToTest("Testing");
+
+        Assertions.assertEquals(listOfStrings.size(), 2);
+
+        Assertions.assertTrue(listOfStrings.contains("test"));
+        Assertions.assertTrue(listOfStrings.contains("testing"));
+    }
 }
